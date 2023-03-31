b9c05a14c6d247fbe24b842105ab25f6f87d54f3
Andrea Staten <statenandrea33@gmail.com>
2023-03-31 16:01:39 -0400

    Test fix

--- crimpy/crimpy.py
+++ crimpy/crimpy.py
@@ -35,39 +35,39 @@ class Map(ipyleaflet.Map):
         self.add_control(draw_control)
 
 
-    def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
-        """Generate a random string of a given length.
+def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
+    """Generate a random string of a given length.
         
-        Args:
-            length (int, optional): The length of the string. Defaults to 10.
-            upper (bool, optional): Whether to include uppercase letters. Defaults to False.
-            digits (bool, optional): Whether to include digits. Defaults to False.
-            punctuation (bool, optional): Whether to include punctuation. Defaults to False.
+    Args:
+        length (int, optional): The length of the string. Defaults to 10.
+        upper (bool, optional): Whether to include uppercase letters. Defaults to False.
+        digits (bool, optional): Whether to include digits. Defaults to False.
+        punctuation (bool, optional): Whether to include punctuation. Defaults to False.
 
-        Returns:
-            str: The generated string.
-        """
+    Returns:
+        str: The generated string.
+    """
         
-        letters = string.ascii_lowercase
-        if upper:
-            letters += string.ascii_uppercase
-        if digits:
-            letters += string.digits
-        if punctuation:
-            letters += string.punctuation
-        result_str = ''.join(random.choice(letters) for i in range(length))
-        return result_str
+    letters = string.ascii_lowercase
+    if upper:
+        letters += string.ascii_uppercase
+    if digits:
+        letters += string.digits
+    if punctuation:
+        letters += string.punctuation
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
     
 
-    def generate_lucky_number(length=1):
-        """Generate a random number of a given length.
+def generate_lucky_number(length=1):
+    """Generate a random number of a given length.
 
-        Args:
-            length (int, optional): The length of the number. Defaults to 1.
+    Args:
+        length (int, optional): The length of the number. Defaults to 1.
         
-        Returns:
-            int: The generated number.
-        """
+    Returns:
+        int: The generated number.
+    """
 
-        result_str = ''.join(random.choice(string.digits) for i in range(length))
-        return int(result_str)
+    result_str = ''.join(random.choice(string.digits) for i in range(length))
+    return int(result_str)
