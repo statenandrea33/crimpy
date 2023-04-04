7c9e940843640da42e7eaa7ae9828dc11b38cd7d
Andrea Staten <statenandrea33@gmail.com>
2023-04-04 10:24:34 -0400

    Added a locations_to_map function

--- crimpy/crimpy.py
+++ crimpy/crimpy.py
@@ -6,12 +6,16 @@ import ipyleaflet
 
 class Map(ipyleaflet.Map):
 
-    def __init__(self,center, zoom, **kwargs) -> None:
-
+    def __init__(self, zoom, **kwargs) -> None:
+        """Adds the ability to use a mouse to zoom in and out.
+        
+        Args:
+            **kwargs: Keyword arguments passed to the scroll wheel zoom.
+        """
         if "scroll_wheel_zoom" not in kwargs:
             kwargs["scroll_wheel_zoom"] = True
 
-        super().__init__(center=center, zoom=zoom, **kwargs)
+        super().__init__(zoom=zoom, **kwargs)
 
     def add_search_control(self, position="topleft", **kwargs):
         """Add a search control to the map.
@@ -34,6 +38,37 @@ class Map(ipyleaflet.Map):
         draw_control = ipyleaflet.DrawControl(**kwargs)
         self.add_control(draw_control)
 
+def add_locations_to_map(m, locations):
+    """Takes coordinates from a list called locations and creates points on a map.
+
+    Parameters:
+        m : The map to add the locations to.
+        locations : A list of locations containing name, latitude, and longitude data. 
+    """
+
+    # Create a marker cluster layer to group nearby markers
+    marker_cluster = ipyleaflet.MarkerCluster()
+
+    # Loop through the list of locations and add a marker for each one
+    for location in locations:
+       # Extract the latitude and longitude from the list
+        lat, lon = location['latitude'], location['longitude']
+       
+        # Create a new marker at the location and add it to the layer
+        marker = ipyleaflet.Marker(location=(lat,lon))
+        m.add_layer(marker)
+    
+    # Add the marker cluster to the map
+    m.add_layer(marker_cluster)
+    
+    # Find the Center of the markers
+    lats = [location['latitude'] for location in locations]
+    lons = [location['longitude'] for location in locations]
+    center_lat = sum(lats) / len(lats)
+    center_lon = sum(lons) / len(lons)
+
+    # Set the center
+    m.center = (center_lat, center_lon)
 
 def generate_random_string(length=10, upper=False, digits=False, punctuation=False):
     """Generate a random string of a given length.
